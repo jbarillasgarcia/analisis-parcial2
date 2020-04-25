ebcf809fd478315bd40fd861885e1dd4cbc2a08e
usr_gsm <cgarciam@tigo.com.gt>
2020-04-24 23:39:38 -0600

    Se han removido comentarios para incrementar la legibilidad.

--- parcial2/src/gt/edu/umg/ingenieria/sistemas/analisis2/parcial2/view/MainForm.java
+++ parcial2/src/gt/edu/umg/ingenieria/sistemas/analisis2/parcial2/view/MainForm.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gt.edu.umg.ingenieria.sistemas.analisis2.parcial2.view;
 
 import gt.edu.umg.ingenieria.sistemas.analisis2.parcial2.entity.ServiceOrderEntity;
@@ -12,10 +7,6 @@ import gt.edu.umg.ingenieria.sistemas.analisis2.parcial2.service.IBrakeRepairSer
 import gt.edu.umg.ingenieria.sistemas.analisis2.parcial2.service.IVehicleReceiptService;
 import gt.edu.umg.ingenieria.sistemas.analisis2.parcial2.service.VehicleReceipt;
 
-/**
- *
- * @author joshi
- */
 public class MainForm extends javax.swing.JFrame {
 
     public MainForm() {
